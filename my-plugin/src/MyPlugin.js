8c81ccd810c4ddfc6df399bc92db3ec4c22c8f68
Eccenux <eccenux+git@gmail.com>
2020-11-02 00:18:45 +0100

    base render

--- my-plugin/src/MyPlugin.js
+++ my-plugin/src/MyPlugin.js
@@ -21,13 +21,29 @@ class MyPlugin {
 	}
 
 	/**
+	 * When portal was selected.
+	 * @param {Object} portal Selected portal POI data.
+	 */
+	onSelectedPortal(portal) {
+		console.log(this.codeName, 'onSelectedPortal', portal);
+		console.log(portal.title, this.playerName, portal.location.latitude, portal.location.longitude);
+	}
+
+	/**
 	 * Hook into view scope.
 	 * @param {Object} scope Angular scope.
 	 */
 	setupViews(scope) {
-		console.log(this.codeName, 'setupViews', scope, scope.setSelectedWaypoint);
+		console.log(this.codeName, 'setupViews', scope.setSelectedWaypoint);
+		var previous = scope.setSelectedWaypoint;
+		scope.setSelectedWaypoint = (waypoint, d) => {
+			//console.log(this.codeName, 'hacked setSelectedWaypoint', {waypoint, d});
+			if (waypoint && waypoint.poi_type && waypoint.poi_type == "PORTAL") {
+				this.onSelectedPortal(waypoint._poi);
+			}
+			previous.call(scope, waypoint, d);
+		};
 	}
-
 
 	/**
 	 * Setup player data.
