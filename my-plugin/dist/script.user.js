610aa7518e7c32164e9ca5f095356234e88e456a
Eccenux <eccenux+git@gmail.com>
2020-11-01 22:06:14 +0100

    initial plugin test

--- my-plugin/dist/script.user.js
+++ my-plugin/dist/script.user.js
@@ -1,17 +1,12 @@
 // ==UserScript==
 // @author      Eccenux
-// @name        IITC plugin: Nux termplate
-// @id          iitc-plugin--nux-termplate
+// @name        FS Puzzle helper for Mission Editor
+// @id          ingress-mission-fs-puzzle-helper
 // @category    Misc
-// @namespace   pl.enux.iitc
+// @namespace   pl.enux.ingress
 // @version     0.0.1
-// @description [0.0.1] Nux template
-// @match       https://*.ingress.com/intel*
-// @match       http://*.ingress.com/intel*
-// @match       https://*.ingress.com/mission/*
-// @match       http://*.ingress.com/mission/*
-// @match       https://intel.ingress.com/*
-// @match       https://intel.ingress.com/*
+// @description [0.0.1] FS Puzzle helper for Mission Editor
+// @match       https://missions.ingress.com/edit*
 // @grant       none
 // ==/UserScript==
 
@@ -29,56 +24,16 @@ class MyPlugin {
 }
 /* eslint-disable no-undef */
 
-// WARNING!!! Change `examplePluginCodeName` to a unique code name of the plugin.
+// WARNING!!! Change `missionFsPuzzleHelper` to a unique code name of the plugin.
 
-let myPlugin = new MyPlugin('examplePluginCodeName');
+let myPlugin = new MyPlugin('missionFsPuzzleHelper');
 
 // ensure plugin framework is there, even if iitc is not yet loaded
 if(typeof window.plugin !== 'function') window.plugin = function() {};
 
 //use own namespace for plugin
-window.plugin.examplePluginCodeName = myPlugin;
+window.plugin.missionFsPuzzleHelper = myPlugin;
 
-//////////////////////////////////////////////////////////////////////
-//WRAPPER START //////////////////////////////////////////////////////
+//window.plugin.missionFsPuzzleHelper.setup();
 
-/**
- * IITC plugin wrapper.
- * 
- * Note! The `wrapper` is injected directly to the Ingress Intel web page.
- * That is why you need to use `window.plugin.examplePluginCodeName` at least for hooks setup.
- */
-function wrapper(plugin_info) {
-
-	//////////////////////////////////////////////////////////////////////
-	//PLUGIN START ///////////////////////////////////////////////////////
-
-	/**
-	 * Some setup (when iitc is ready)
-	 * 
-	 * See notes for the wrapper!
-	 */
-	function setup() {
-		console.log('examplePluginCodeName - init')
-		window.plugin.examplePluginCodeName.setup();
-	}
-
-	//PLUGIN END /////////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////////
-
-	setup.info = plugin_info; //add the script info data to the function as a property
-	if(!window.bootPlugins) window.bootPlugins = [];
-	window.bootPlugins.push(setup);
-	// if IITC has already booted, immediately run the 'setup' function
-	if(window.iitcLoaded && typeof setup === 'function') setup();
-}
-
-//WRAPPER END ////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-
-// inject code into site context
-var script = document.createElement('script');
-var info = {};
-if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
-script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
-(document.body || document.head || document.documentElement).appendChild(script);
+console.log('missionFsPuzzleHelper - test', angular, window.angular, unsafeWindow.angular);
--- my-plugin/src/index.js
+++ my-plugin/src/index.js
@@ -1,55 +1,15 @@
 ﻿/* eslint-disable no-undef */
 
-// WARNING!!! Change `examplePluginCodeName` to a unique code name of the plugin.
+// WARNING!!! Change `missionFsPuzzleHelper` to a unique code name of the plugin.
 
-let myPlugin = new MyPlugin('examplePluginCodeName');
+let myPlugin = new MyPlugin('missionFsPuzzleHelper');
 
 // ensure plugin framework is there, even if iitc is not yet loaded
 if(typeof window.plugin !== 'function') window.plugin = function() {};
 
 //use own namespace for plugin
-window.plugin.examplePluginCodeName = myPlugin;
+window.plugin.missionFsPuzzleHelper = myPlugin;
 
-//////////////////////////////////////////////////////////////////////
-//WRAPPER START //////////////////////////////////////////////////////
+//window.plugin.missionFsPuzzleHelper.setup();
 
-/**
- * IITC plugin wrapper.
- * 
- * Note! The `wrapper` is injected directly to the Ingress Intel web page.
- * That is why you need to use `window.plugin.examplePluginCodeName` at least for hooks setup.
- */
-function wrapper(plugin_info) {
-
-	//////////////////////////////////////////////////////////////////////
-	//PLUGIN START ///////////////////////////////////////////////////////
-
-	/**
-	 * Some setup (when iitc is ready)
-	 * 
-	 * See notes for the wrapper!
-	 */
-	function setup() {
-		console.log('examplePluginCodeName - init')
-		window.plugin.examplePluginCodeName.setup();
-	}
-
-	//PLUGIN END /////////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////////
-
-	setup.info = plugin_info; //add the script info data to the function as a property
-	if(!window.bootPlugins) window.bootPlugins = [];
-	window.bootPlugins.push(setup);
-	// if IITC has already booted, immediately run the 'setup' function
-	if(window.iitcLoaded && typeof setup === 'function') setup();
-}
-
-//WRAPPER END ////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-
-// inject code into site context
-var script = document.createElement('script');
-var info = {};
-if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
-script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
-(document.body || document.head || document.documentElement).appendChild(script);
+console.log('missionFsPuzzleHelper - test', angular, window.angular, unsafeWindow.angular);
